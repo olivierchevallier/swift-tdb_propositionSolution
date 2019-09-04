a01d41a71a782f407ff291ae349a0e31864457af
Olivier Chevallier <olivierchevallier@macbook-pro-dolivier.local>
2019-09-04 10:11:49 +0200

    Initial Commit

---
+++ tdb_propositionSolution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tdb_propositionSolution
+//
+//  Created by olivier chevallier on 04.09.19.
+//  Copyright © 2019 Olivier Chevallier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tdb_propositionSolutionTests/tdb_propositionSolutionTests.swift
@@ -0,0 +1,34 @@
+//
+//  tdb_propositionSolutionTests.swift
+//  tdb_propositionSolutionTests
+//
+//  Created by olivier chevallier on 04.09.19.
+//  Copyright © 2019 Olivier Chevallier. All rights reserved.
+//
+
+import XCTest
+@testable import tdb_propositionSolution
+
+class tdb_propositionSolutionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
