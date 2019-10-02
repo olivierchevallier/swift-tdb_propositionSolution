21019ff2e495736d72a39b033e7194bfc24dad57
Olivier Chevallier <olivierchevallier@macbook-pro-dolivier.local>
2019-10-02 19:25:30 +0200

    Affichage correct pour la navigation TbT en TP

--- tdb_propositionSolution/Controllers/TransitItineraryController.swift
+++ tdb_propositionSolution/Controllers/TransitItineraryController.swift
@@ -13,7 +13,7 @@ import MapboxCoreNavigation
 import MapboxNavigation
 import MapboxDirections
 
-class TransitItineraryController: UIViewController {
+class TransitItineraryController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     //MARK: - Properties
     //MARK: Mutable
     var itinerary: TransitItinerary?
@@ -24,11 +24,20 @@ class TransitItineraryController: UIViewController {
     @IBOutlet var lbl_duration: UILabel!
     @IBOutlet var stk_details: UIStackView!
     @IBOutlet var scrl_details: UIScrollView!
+    @IBOutlet var col_navigation: UICollectionView!
     
+    //MARK: Computed
+    private var numberOfSteps: Int {
+        get {
+            return itinerary!.connection.sections.count - 1
+        }
+    }
     
     //MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
+        col_navigation.layer.cornerRadius = 10
+        col_navigation.delegate = self
         showItineraryInfos()
         showItinerarySteps()
     }
@@ -56,5 +65,35 @@ class TransitItineraryController: UIViewController {
             }
             stk_details.addArrangedSubview(step)
         }
+    }
+    
+    //MARK: - Collection view delegate
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfSteps
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "transitNavigationCollectionViewCell", for: indexPath) as! TransitNavigationCollectionViewCell
+        let nextSection = indexPath.row + 1 < numberOfSteps ? itinerary!.connection.sections[indexPath.row + 1] : nil
+        cell.section = itinerary!.connection.sections[indexPath.row]
+        cell.nextSection = nextSection
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = col_navigation.bounds
+        return CGSize(width: size.width, height: size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
     }
 }
--- tdb_propositionSolution/View/TransitNavigationCollectionViewCell.swift
+++ tdb_propositionSolution/View/TransitNavigationCollectionViewCell.swift
@@ -11,8 +11,61 @@ import UIKit
 
 class TransitNavigationCollectionViewCell: UICollectionViewCell {
     //MARK: - Properties
+    //MARK: Mutable
+    var section: TransitWebService.Section? {
+        didSet {
+            if section?.journey != nil {
+                let line = TransitLine(journey: self.section!.journey!)
+                lbl_connection.text = "Descendez du"
+                lbl_connectionLine.line = line
+                lbl_leaveAt.text = " dans x min. à"
+                lbl_leaveAt.isHidden = false
+                lbl_connectionLine.isHidden = false
+            } else {
+                lbl_connection.text = "Marchez jusqu'à "
+                lbl_leaveAt.isHidden = true
+                lbl_connectionLine.isHidden = true
+            }
+            lbl_nextStop.text = TransitItinerary.splitAtFirst(str: section!.arrival.station.name!, delimiter: "@")!.first
+            resizeLabels()
+        }
+    }
+    var nextSection: TransitWebService.Section? {
+        didSet {
+            if self.nextSection != nil {
+                if self.nextSection!.journey != nil {
+                    let line = TransitLine(journey: self.nextSection!.journey!)
+                    lbl_nextConnection.text = "Puis prenez le"
+                    lbl_nextConnectionLine.line = line
+                    lbl_nextConnectionIn.text = "dans x min."
+                    lbl_nextConnectionDirection.text = "→ \(line.destination)"
+                    lbl_nextConnectionLine.isHidden = false
+                    lbl_nextConnectionIn.isHidden = false
+                } else {
+                    lbl_nextConnection.text = "Puis marchez jusqu'à"
+                    lbl_nextConnectionDirection.text = TransitItinerary.splitAtFirst(str: nextSection!.arrival.station.name!, delimiter: "@")!.first
+                    lbl_nextConnectionLine.isHidden = true
+                    lbl_nextConnectionIn.isHidden = true
+                }
+            } else {
+                lbl_nextConnection.text = "Puis marchez jusqu'à"
+                lbl_nextConnectionDirection.text = "Votre destination"
+                lbl_nextConnectionLine.isHidden = true
+                lbl_nextConnectionIn.isHidden = true
+            }
+            resizeLabels()
+        }
+    }
+    
     //MARK: Controls
-    @IBOutlet var lbl_infos: UILabel!
+    @IBOutlet var lbl_connection: UILabel!
+    @IBOutlet var lbl_nextStop: UILabel!
+    @IBOutlet var lbl_nextConnection: UILabel!
+    @IBOutlet var lbl_nextConnectionLine: TransitLineControl!
+    @IBOutlet var lbl_nextConnectionDirection: UILabel!
+    @IBOutlet var lbl_nextConnectionIn: UILabel!
+    @IBOutlet var lbl_connectionLine: TransitLineControl!
+    @IBOutlet var lbl_leaveAt: UILabel!
     
     //MARK: Initialization
     override init(frame: CGRect) {
@@ -21,5 +74,12 @@ class TransitNavigationCollectionViewCell: UICollectionViewCell {
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+    
+    //MARK: - Private methods
+    private func resizeLabels() {
+        lbl_connection.sizeToFit()
+        lbl_nextConnection.sizeToFit()
+        lbl_connection.sizeToFit()
     }
 }
