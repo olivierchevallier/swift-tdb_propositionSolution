a9b7b35c9c844472002a268c6e707dbc734bb642
Olivier Chevallier <olivierchevallier@macbook-pro-dolivier.local>
2019-10-09 17:12:36 +0200

    Affichage des préférences

--- tdb_propositionSolution/Controllers/SettingsViewController.swift
+++ tdb_propositionSolution/Controllers/SettingsViewController.swift
@@ -9,14 +9,65 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsViewController: UIViewController, UITextFieldDelegate {
+    //MARK: - Properties
+    //MARK: Immutable
+    let defaults = UserDefaults.standard
+    
+    //MARK: Mutable
+    var walkSpeed: Double = 4.5
+    var homeAdress: String = ""
+    var electric: Bool = false
+    var consumption: Double = 7.5
+    var weight: Int = 1510
+    
+    //MARK: Controls
+    @IBOutlet var txt_walkSpeed: UITextField!
+    @IBOutlet var txt_homeAdress: UITextField!
+    @IBOutlet var sw_electric: UISwitch!
+    @IBOutlet var txt_consumption: UITextField!
+    @IBOutlet var txt_weight: UITextField!
+    
+    //MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadPreferences()
+        showPreferences()
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
+    //MARK: Private methods
+    private func loadPreferences() {
+        if defaults.bool(forKey: "configured") {
+            walkSpeed = defaults.double(forKey: "walkSpeed")
+            homeAdress = defaults.string(forKey: "homeAdress")!
+        }
+        electric = defaults.bool(forKey: "electric")
+        if !electric {
+            if defaults.double(forKey: "consumption") != 0 {
+                consumption = defaults.double(forKey: "consumption")
+            }
+        }
+        if defaults.integer(forKey: "weight") != 0 {
+            weight = defaults.integer(forKey: "weight")
+        }
+    }
+    
+    private func showPreferences() {
+        txt_walkSpeed.text = String(walkSpeed)
+        txt_homeAdress.text = homeAdress
+        sw_electric.isOn = electric
+        txt_consumption.text = String(consumption)
+        txt_weight.text = String(weight)
+    }
+    
+    //MARK: - UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        print("rupestre")
+        return true
+    }
 
     /*
     // MARK: - Navigation
