2629014cd2f11f9ccd514a2dc61ce70d52b0d599
Olivier Chevallier <olivierchevallier@macbook-pro-dolivier.local>
2019-10-09 16:46:42 +0200

    Interface des préférences

---
+++ tdb_propositionSolution/Controllers/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//--------------------------------------------------
+// Travail de bachelor - Proposition de solution
+//
+// SettingsViewController :
+//
+// Créé par : Olivier Chevallier le 09.10.19
+//--------------------------------------------------
+
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
